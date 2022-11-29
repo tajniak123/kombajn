6bb4289ac89bc10a1c9f3103be397404f5f17a20
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-11-29 16:04:25 +0100

    Update main.py - code review.

--- model.py
+++ model.py
@@ -3,49 +3,56 @@ import pandas
 import time
 from colorama import Fore
 from colorama import Style
-import os
+from strings import (RESULTS_NOT_FOUND,
+                     DATABASE_FILE_NAME,
+                     NIST_KEY,
+                     NIST_KEYWORD_SEARCH,
+                     SEVERITY_CRITICAL,
+                     SEVERITY_HIGH,
+                     SEVERITY_MEDIUM,
+                     SEVERITY_LOW)
+
 
 class Model:
-    def __init__(self):
-
-
-        # os.system('python -m pip install --upgrade pip setuptools virtualenv')
-        #os.system('pip install pyqt5')
-
-        start_time = time.time()
-
-        print(50 * "=")
-        plik = pandas.read_excel('Zeszyt1.xlsx', usecols=[0])
-
-        a = nvdlib.searchCVE(keywordSearch='oneconnect', key='2b1f6592-3b28-43f6-819e-8b0281c3dc1c')
-
-        colorDict = {
-            "CRITICAL": Fore.LIGHTRED_EX,
-            "HIGH": Fore.RED,
-            "MEDIUM": Fore.YELLOW,
-            "LOW": Fore.GREEN,
+    severity_to_colors_converter = {
+            SEVERITY_CRITICAL: Fore.LIGHTRED_EX,
+            SEVERITY_HIGH: Fore.RED,
+            SEVERITY_MEDIUM: Fore.YELLOW,
+            SEVERITY_LOW: Fore.GREEN,
             None: Fore.MAGENTA
         }
 
-        c = 1
+    def __init__(self):
+        start_time = time.time()
 
-        for i in range(len(plik)):
+        print(50 * "=")
+        database = pandas.read_excel(DATABASE_FILE_NAME, usecols=[0])
+
+        a = nvdlib.searchCVE(keywordSearch=NIST_KEYWORD_SEARCH, key=NIST_KEY)
+        counter = 1
+
+        for i in range(len(database)):
             start_time1 = time.time()
-            print(f'{c}. {plik.iloc[i, 0]}')
-            a = nvdlib.searchCVE(keywordSearch=plik.iloc[i, 0], key='2b1f6592-3b28-43f6-819e-8b0281c3dc1c')
+
+            print(f'{counter}. {database.iloc[i, 0]}')
+
+            a = nvdlib.searchCVE(keywordSearch=database.iloc[i, 0], key=NIST_KEY)
             if len(a) == 0:
-                print(f'{Fore.RED}Nie znaleziono wyników{Style.RESET_ALL}')
+                print(f'{Fore.RED}{RESULTS_NOT_FOUND}{Style.RESET_ALL}')
             else:
                 for j in a:
-                    print(
-                        f'{Fore.BLUE} {j.id} {Style.RESET_ALL} score={j.score[1]} severity={colorDict[j.score[2]]}{j.score[2]} {Style.RESET_ALL}')
+                    id_s = f'{Fore.BLUE}{j.id}{Style.RESET_ALL}'
+                    score_s = f'score={j.score[1]}'
+                    severity_color = self.severity_to_colors_converter[j.score[2]]
+                    scores_s = f'severity={severity_color}{j.score[2]}{Style.RESET_ALL}'
 
-            c += 1
+                    print(f'{id_s} {score_s}\t\t{scores_s}')
+
+            counter += 1
             end_time1 = time.time()
+
             print("--- %s seconds ---" % round(end_time1 - start_time1, 2))
             print(50 * "=")
 
         end_time = time.time()
         print("--- %s seconds ---" % (end_time - start_time))
-
-        plik = pandas.read_excel('Zeszyt1.xlsx', usecols=[0])
